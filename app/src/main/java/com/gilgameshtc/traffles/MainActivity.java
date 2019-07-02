ef0455ce96835b8ed3de96eb4ed4181514a4e5f1
TravisCJY <travisching007@gmail.com>
2019-07-02 18:04:37 +0800

    Add budget text
    Database currently uses User Preference.

--- app/src/main/java/com/gilgameshtc/traffles/MainActivity.java
+++ app/src/main/java/com/gilgameshtc/traffles/MainActivity.java
@@ -1,14 +1,18 @@
 package com.gilgameshtc.traffles;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.Window;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button budget;
+    private SharedPreferences savedSettings;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,17 +31,27 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    // Precondition: Should only be called after budget button and userPref have been initialized.
+    private void setupBudget() {
+        String expenditureSoFar = savedSettings.getString("budget", "0");
+        budget.setText(expenditureSoFar);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Title Bar configuration
         requestWindowFeature(Window.FEATURE_NO_TITLE); // hide the title
         getSupportActionBar().hide(); // hide the title bar
-
+        // Set up lay out
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        // Get user saved data
+        savedSettings = getSharedPreferences("userPref", MODE_PRIVATE);
+        // Capture Budget Text button
+        budget = findViewById(R.id.budgetText);
+        setupBudget();
+        // Capture Bottom Navigation Bar (currently hidden)
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 }
