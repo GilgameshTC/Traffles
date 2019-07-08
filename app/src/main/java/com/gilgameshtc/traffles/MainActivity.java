79fe6264e0484c78d41a86b50aac563acd896a76
TravisCJY <travisching007@gmail.com>
2019-07-08 16:05:40 +0800

    Integrated CircularProgressBar into MainActivity

--- app/src/main/java/com/gilgameshtc/traffles/MainActivity.java
+++ app/src/main/java/com/gilgameshtc/traffles/MainActivity.java
@@ -1,7 +1,6 @@
 package com.gilgameshtc.traffles;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
